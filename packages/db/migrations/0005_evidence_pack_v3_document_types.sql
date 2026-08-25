bbb29049a8859baf1fd9b196802f5efda49b6135
huynhlongdai <itroot2015@gmail.com>
2026-08-25 12:37:07 +0700

    TH-115: allow Evidence Pack v3 document types

---
+++ packages/db/migrations/0005_evidence_pack_v3_document_types.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+ALTER TABLE source_documents DROP CONSTRAINT IF EXISTS source_documents_document_type_check;
+
+ALTER TABLE source_documents
+ADD CONSTRAINT source_documents_document_type_check
+CHECK (document_type IN ('readme','documentation','package','other','repository_tree','manifest','container','configuration','contributing','security','architecture','ci','source_entrypoint','changelog'));
+
+COMMIT;
